a01ffe4591dfadd9a103b5630af9eacf2fe8ad7c
renrui <renrui@gamil.com>
2020-06-03 17:33:00 +0800

    general devices

--- api/src/main/java/com/paisiwater/controller/MiniProgramController.java
+++ api/src/main/java/com/paisiwater/controller/MiniProgramController.java
@@ -1,10 +1,12 @@
 package com.paisiwater.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.paisi.utils.CookieUtil;
 import com.paisi.utils.StrUtil;
 import com.paisiwater.api.controller.constant.WeixinConstant;
 import com.paisiwater.api.model.DeviceInfo;
 import com.paisiwater.api.model.FilterInfo;
+import com.paisiwater.api.model.GeneralDeviceInfo;
 import com.paisiwater.model.WxBindInfo;
 import com.paisiwater.model.WxUserInfo;
 import com.paisiwater.service.WeixinService;
@@ -38,13 +40,13 @@ public class MiniProgramController {
 
     @RequestMapping(value = "get_bind_device_info")
     @ResponseBody
-    public String getDeviceInfoByUnionId(HttpServletRequest request, @RequestParam("unionId") String unionId, Model model) {
-        logger.info("unionId: " + unionId);
+    public String getDeviceInfoByUnionId(HttpServletRequest request, @RequestParam("deviceId") String deviceId, @RequestParam("unionId") String unionId, Model model) {
         Map<String, String> zInfo = new HashMap<>();
         try {
             WxUserInfo wxUserInfo = weixinService.getWxUserInfoByUnionId(unionId);
             WxBindInfo wxBindInfo = new WxBindInfo();
             wxBindInfo.setOpenid(wxUserInfo.getOpenid());
+            wxBindInfo.setDeviceId(deviceId);
             wxBindInfo.setAppId(WeixinConstant.APP_ID);
             wxBindInfo = weixinService.getWxBindInfoByDevice(wxBindInfo);
             DeviceInfo deviceInfo = null;
@@ -53,12 +55,13 @@ public class MiniProgramController {
                 deviceInfo = weixinService.getDeviceInfo(wxBindInfo.getDeviceId());
                 if (deviceInfo != null) {
                     filterInfos = weixinService.getFilterInfo(deviceInfo.getModel());
-                    zInfo.put("unionid", wxUserInfo.getUnionid());
+//                    zInfo.put("unionid", wxUserInfo.getUnionid());
                     zInfo.put("deviceId", deviceInfo.getDeviceId());
                     zInfo.put("seqNum", deviceInfo.getSeqNum());
                     zInfo.put("model", deviceInfo.getModel());
                     String filters = JSON.toJSONString(filterInfos);
                     zInfo.put("filters", filters);
+                    logger.info("infeo: " + JSON.toJSONString(zInfo));
                     return JSON.toJSONString(zInfo);
                 }
             }
@@ -97,4 +100,26 @@ public class MiniProgramController {
         }
         return "";
     }
+
+    @RequestMapping(value = "get-general-list")
+    @ResponseBody
+    public String getGeneralBindInfo(HttpServletRequest request, @RequestParam("unionId") String unionId, Model model) throws Exception {
+        try {
+            WxUserInfo wxUserInfo = weixinService.getWxUserInfoByUnionId(unionId);
+            List<GeneralDeviceInfo> generalDeviceInfos;
+            GeneralDeviceInfo info = new GeneralDeviceInfo();
+            info.setOpen_id(wxUserInfo.getOpenid());
+            info.setApp_id(WeixinConstant.APP_ID);
+            generalDeviceInfos = weixinService.getGeneralBindInfo(info);
+            if (generalDeviceInfos != null) {
+                Map<String, String> result = new HashMap<>();
+                result.put("generalList", JSON.toJSONString(generalDeviceInfos));
+                return JSON.toJSONString(result);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return null;
+    }
+
 }
